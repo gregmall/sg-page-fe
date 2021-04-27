0d337888e4b73f869e338cc79fbc546d2479492f
Greg Mall <gregmall157@gmail.com>
2021-04-26 17:28:45 -0700

    styling

--- src/components/SideNav/SideNav.jsx
+++ src/components/SideNav/SideNav.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import styles from './SideNav.scss'
 export default function SideNav() {
   return (
+    <>
     <div className={styles.sidebar}>
       <aside>
       <nav className={styles.nav}>
           
-          <ul className={styles.active}>
+          <ul>
             <li><a href="/home">Home</a></li>
             <li><a href="/about">About</a></li>
             <li><a href="/glass">Glass</a></li>
@@ -17,5 +18,17 @@ export default function SideNav() {
         </nav>
       </aside>
     </div>
+    <footer className={styles.footer}>
+   
+    <section className={styles.navMobile}>
+            <span><a href="/home">Home</a></span>
+            <span><a href="/about">About</a></span>
+            <span><a href="/glass">Glass</a></span>
+            <span><a href="https://www.etsy.com/shop/SynthesisGlass" target="blank">Buy</a></span>
+            <span><a href="/contact" target ="_blank">Contact</a></span>
+    </section>
+    
+    </footer>
+    </>
   )
 }
